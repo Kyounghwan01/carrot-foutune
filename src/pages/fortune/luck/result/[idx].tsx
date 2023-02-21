cd3d8b90a36ee872d694a17a9ca7dfd2584f0a68
kyounghwan Noh <noh5524@gmail.com>
2023-02-21 06:57:30 +0000

    운세 로딩화면 추가

--- src/pages/fortune/luck/result/[idx].tsx
+++ src/pages/fortune/luck/result/[idx].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GetStaticPaths } from "next";
 import Router from "next/router";
 import Header from "@/components/Header";
@@ -25,30 +25,52 @@ const paths = Array(5)
     return { params: { idx: String(i + 1) } };
   });
 
-const Index = ({luck}: {luck: ILuck}) => {
+const Index = ({ luck }: { luck: ILuck }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div>
-      <Header isBack={true} title={"오늘의 운세"} />
-      {JSON.stringify(luck)}
-      {/* todo: loading페이지, 운세 result 페이지 css */}
+    <>
+      {isLoading ? (
+        <LoadingWarpper>
+          <div className="wrap">
+            <img src="/fortune-loading.png" width="190px" height="147px" />
+            <p>
+              대길이가 오늘의 행운을<br />
+              가져오고 있어요!
+            </p>
+          </div>
+        </LoadingWarpper>
+      ) : (
+        <div>
+          <Header isBack={true} title={"오늘의 운세"} />
+          {JSON.stringify(luck)}
+          {/* todo: loading페이지, 운세 result 페이지 css */}
 
-      <Content>
-        <Title>
-          <span>
-            오늘의 운세를 확인하고
-            <br />
-            행운의 기운을 활용하세요!
-          </span>
-          <img src="/fortune-title.png" alt="" />
-        </Title>
-      </Content>
+          <Content>
+            <Title>
+              <span>
+                오늘의 운세를 확인하고
+                <br />
+                행운의 기운을 활용하세요!
+              </span>
+              <img src="/fortune-title.png" alt="" />
+            </Title>
+          </Content>
 
-      <BottomFixedButton
-        title={"확인"}
-        disabled={false}
-        onClick={() => Router.push("/fortune")}
-      />
-    </div>
+          <BottomFixedButton
+            title={"확인"}
+            disabled={false}
+            onClick={() => Router.push("/fortune")}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
@@ -56,6 +78,31 @@ const Content = styled.div`
   padding: 80px 20px 80px;
 `;
 
+
+const LoadingWarpper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .wrap {
+    position: relative;
+  }
+  img {
+    position: relative;
+    left: 100px;
+    transform: translateX(-200px);
+    transition: all 1s ease-in;
+  }
+  p {
+    margin-top: 20px;
+    text-align: center;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -1px;
+  }
+`
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
@@ -72,5 +119,3 @@ export async function getStaticProps({ params }: { params: ILuck }) {
 
 
 export default Index;
-
-// todo:  staticprops로 idx에 있는값 json에서 불러와서 가져오기
